7bbeaecf5e4d8ceb47526c1d06d5b34cbd6aa9f8
JuanpDiaz02 <165917191+JuanpDiaz02@users.noreply.github.com>
2025-08-14 23:00:30 -0300

    Update urls.py

--- finalblog/urls.py
+++ finalblog/urls.py
@@ -7,16 +7,14 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Rutas principales
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
 
-    # Apps
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('pages/', include(('pages.urls', 'pages'), namespace='pages')),
     path('messages/', include(('messaging.urls', 'messaging'), namespace='messaging')),
 ]
 
-# Archivos media
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
